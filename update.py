596bea6df2fc91814fe70c77e1d4a60afccd6a0b
Emre Ozfatura <48322751+emre1925@users.noreply.github.com>
2021-01-18 23:56:58 +0000

    Update update.py

--- update.py
+++ update.py
@@ -1,13 +1,19 @@
-def global_update_normalize(local_diff,max_diff_norm,args):
+def global_update_normalized(local_diff,max_diff_norm,args):
     # local_differences contains the local model diffrence of the workers.
     # max_diff_norm is the maximum norm value allowed for each local model diffrence.
     ############ Here we nomalize the local diffrences###################
     for i in range (args.numb_worker)
+    ####################### Global diffrence is the average of normalized local diffrences ##########################################
         global_diff+= (1/arg.numb_worker) local_diff[i] * (minimum(torch.norm(local_diff[i]),max_diff_norm)/torch.norm(local_diff[i]))
         max_diff_norm_new+= (1/arg.numb_worker)torch.norm(local_diff[i])
     max_diff_norm_new = max_diff_norm_new * args.norm_coeff + max_diff_norm (1-args.norm_coeff)
-    return max_diff_norm_new, global_diff
+    return max_diff_norm_new , global_diff
 
-def global_update_relaxed_average(local_differences, global_difference_prev,args):
-    
-    return  
+def global_update_relaxed_average(local_diff, global_diff_prev,global_model,args):
+     for i in range (args.numb_worker)
+    ####################### Global diffrence is the average of  local diffrences ##########################################
+        global_diff+= (1/arg.numb_worker) local_diff[i]
+     global_model = global_model + global_diff
+    return  global_model
+def local_update_relaxed_average()
+    #Burda local model update edilcek###########
