cd33b6649cff2c8fa9052069d815584730fae5cf
Emre Ozfatura <48322751+emre1925@users.noreply.github.com>
2021-01-18 23:27:29 +0000

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,13 @@
+def global_update_normalize(local_diff,max_diff_norm,args):
+    # local_differences contains the local model diffrence of the workers.
+    # max_diff_norm is the maximum norm value allowed for each local model diffrence.
+    ############ Here we nomalize the local diffrences###################
+    for i in range (args.numb_worker)
+        global_diff+= (1/arg.numb_worker) local_diff[i] * (minimum(torch.norm(local_diff[i]),max_diff_norm)/torch.norm(local_diff[i]))
+        max_diff_norm_new+= (1/arg.numb_worker)torch.norm(local_diff[i])
+    max_diff_norm_new = max_diff_norm_new * args.norm_coeff + max_diff_norm (1-args.norm_coeff)
+    return max_diff_norm_new, global_diff
 
-def pull_model(model_user, model_server):
-    for param_user, param_server in zip(model_user.parameters(), model_server.parameters()):
-        param_user.data = param_server.data[:] + 0
-    return None
+def global_update_relaxed_average(local_differences, global_difference_prev,args):
+    
+    return  
