ecf6510c8eebdd63026995598fc25373c2523b77
Kerem <keremozfatura@me.com>
2021-01-22 23:48:16 +0300

    adaptable alpha update

--- parameters.py
+++ parameters.py
@@ -38,7 +38,7 @@ def args_parser():
     parser.add_argument('--synch', type=bool, default=False, help='enable model synch at alfa change')
     parser.add_argument('--warmUp', type=bool, default=True, help='LR warm up.')
     
-    parser.add_argument('--WarmEpoch', type=int, default=5 help='Warmup_duration.')
+    parser.add_argument('--WarmEpoch', type=int, default=5, help='Warmup_duration.')
 
     args = parser.parse_args()
     return args
--- update.py
+++ update.py
@@ -1,21 +1,25 @@
 #Model update method with normalization
-def global_update_normalized(local_diff,max_diff_norm,args):
+import torch
+def global_update_normalized(local_diffs,max_diff_norm,device,args):
     # local_differences contains the local model diffrence of the workers.
     # max_diff_norm is the maximum norm value allowed for each local model diffrence.
+    #local diff set of difs
     ############ Here we nomalize the local diffrences###################
-    for i in range (args.numb_worker):
+    global_diff = torch.zeros_like(local_diffs[0], device=device)
+    max_diff_norm_new = torch.zeros_like(local_diffs[0], device=device)
+    for dif in local_diffs:
     ####################### Global diffrence is the average of normalized local diffrences ##########################################
-        global_diff+= (1/arg.numb_worker) local_diff[i] * (minimum(torch.norm(local_diff[i]),max_diff_norm)/torch.norm(local_diff[i]))
-        max_diff_norm_new+= (1/arg.numb_worker)torch.norm(local_diff[i])
+        global_diff = global_diff.add(dif* min([torch.norm(dif),max_diff_norm]) / torch.norm(dif),alpha=1/args.num_client)
+        max_diff_norm_new = max_diff_norm_new.add(torch.norm(dif),alpha=1/args.num_client)
     max_diff_norm_new = max_diff_norm_new * args.norm_coeff + max_diff_norm (1-args.norm_coeff)
     return max_diff_norm_new , global_diff
 # Model update method with relaxed averaging
-def global_update_relaxed_average(local_diff, global_diff_prev,global_model,args):
-     for i in range (args.num_client):
-    ####################### Global diffrence is the average of  local diffrences ##########################################
-        global_diff+= (1/arg.numb_worker) local_diff[i]
-     global_model = global_model + global_diff
-    return  global_model
+def global_update_relaxed_average(local_diffs,global_model,device,args):
+     global_diff = torch.zeros_like(global_model,device=device)
+     for dif in local_diffs:
+        global_diff =global_diff.add(dif , alpha=1/args.num_client)
+     global_model = global_model.add(global_diff, alpha=1)
+     return global_model
 # Local Model update with relaxed averaging
 def local_update_relaxed_average():
     #Burda local model update edilcek###########
